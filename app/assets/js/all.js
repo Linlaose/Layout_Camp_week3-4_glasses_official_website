5ae54585b6078aa9b3fa128b4c6517514d3a79bb
您的姓名 <98501671+Linlaose@users.noreply.github.com>
2022-07-19 15:37:07 +0800

    series 完成

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,26 @@
-console.log('Hello!');
+console.log('hello');
 
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
+// series block
+const seriesItem = document.querySelector('.seriesItem');
+const opticalTitle = document.querySelector('.optical');
+const sunglassesTitle = document.querySelector('.sunglasses');
+const functionalTitle = document.querySelector('.functional');
+
+seriesItem.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (e.target.textContent === 'OPTICAL') {
+    opticalTitle.setAttribute('class', 'border-show');
+    sunglassesTitle.removeAttribute('class');
+    functionalTitle.removeAttribute('class');
+  }
+  else if (e.target.textContent === 'SUNGLASSES') {
+    sunglassesTitle.setAttribute('class', 'border-show');
+    opticalTitle.removeAttribute('class');
+    functionalTitle.removeAttribute('class');
+  }
+  else {
+    functionalTitle.setAttribute('class', 'border-show');
+    opticalTitle.removeAttribute('class');
+    sunglassesTitle.removeAttribute('class');
+  }
 });

d274316180f606880b3ecc72a3f7253ff3671182
您的姓名 <98501671+Linlaose@users.noreply.github.com>
2022-07-19 20:30:56 +0800

    optical 區塊  product 完成

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -6,21 +6,22 @@ const opticalTitle = document.querySelector('.optical');
 const sunglassesTitle = document.querySelector('.sunglasses');
 const functionalTitle = document.querySelector('.functional');
 
+// 觸發產品頁籤時加上 border
 seriesItem.addEventListener('click', (e) => {
   e.preventDefault();
   if (e.target.textContent === 'OPTICAL') {
-    opticalTitle.setAttribute('class', 'border-show');
-    sunglassesTitle.removeAttribute('class');
-    functionalTitle.removeAttribute('class');
+    opticalTitle.setAttribute('class', 'borderShow');
+    sunglassesTitle.setAttribute('class', 'borderTransparent');
+    functionalTitle.setAttribute('class', 'borderTransparent');
   }
   else if (e.target.textContent === 'SUNGLASSES') {
-    sunglassesTitle.setAttribute('class', 'border-show');
-    opticalTitle.removeAttribute('class');
-    functionalTitle.removeAttribute('class');
+    sunglassesTitle.setAttribute('class', 'borderShow');
+    opticalTitle.setAttribute('class', 'borderTransparent');
+    functionalTitle.setAttribute('class', 'borderTransparent');
   }
   else {
-    functionalTitle.setAttribute('class', 'border-show');
-    opticalTitle.removeAttribute('class');
-    sunglassesTitle.removeAttribute('class');
+    functionalTitle.setAttribute('class', 'borderShow');
+    opticalTitle.setAttribute('class', 'borderTransparent');
+    sunglassesTitle.setAttribute('class', 'borderTransparent');
   }
 });

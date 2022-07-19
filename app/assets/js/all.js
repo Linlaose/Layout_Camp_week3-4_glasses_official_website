f77b8e30c8a7824b1e4c56e8334c28e3f4508ec3
您的姓名 <98501671+Linlaose@users.noreply.github.com>
2022-07-19 22:54:19 +0800

    sunglasses 按鈕切換產品列表完成

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -5,6 +5,12 @@ const seriesItem = document.querySelector('.seriesItem');
 const opticalTitle = document.querySelector('.optical');
 const sunglassesTitle = document.querySelector('.sunglasses');
 const functionalTitle = document.querySelector('.functional');
+// 經過 querySelectorAll 選取的變數會被儲存為陣列，使用時注意，不可視為單一元素
+const opticalImage = document.querySelectorAll('.opticalImageHeader');
+const celluloid = document.querySelector('.titleCelluloid');
+const opticalList = document.querySelector('.opticalProductList');
+const sunglassesList = document.querySelector('.sunglassesList');
+
 
 // 觸發產品頁籤時加上 border
 seriesItem.addEventListener('click', (e) => {
@@ -13,11 +19,23 @@ seriesItem.addEventListener('click', (e) => {
     opticalTitle.setAttribute('class', 'borderShow');
     sunglassesTitle.setAttribute('class', 'borderTransparent');
     functionalTitle.setAttribute('class', 'borderTransparent');
+    opticalImage.forEach((i) => {
+      i.removeAttribute('class');
+    });
+    celluloid.textContent = 'Celluloid Combi';
+    opticalList.setAttribute('class', 'opticalProductList');
+    sunglassesList.setAttribute('class', 'listHide');
   }
   else if (e.target.textContent === 'SUNGLASSES') {
     sunglassesTitle.setAttribute('class', 'borderShow');
     opticalTitle.setAttribute('class', 'borderTransparent');
     functionalTitle.setAttribute('class', 'borderTransparent');
+    opticalImage.forEach((i) => {
+      i.setAttribute('class', 'imgHide');
+    });
+    celluloid.textContent = '2020 NEW COLLECTION';
+    opticalList.setAttribute('class', 'listHide');
+    sunglassesList.setAttribute('class', 'sunglassesProductList');
   }
   else {
     functionalTitle.setAttribute('class', 'borderShow');

4e2a74e6dea19b8bf30418ebc22f490fa50d9516
您的姓名 <98501671+Linlaose@users.noreply.github.com>
2022-07-19 15:37:38 +0800

    Update 2022-07-19T07:37:31.943Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,26 @@
 "use strict";
 
-console.log('Hello!');
-$(document).ready(function () {
-  console.log('HesSchool Hello!');
+console.log('hello'); // series block
+
+var seriesItem = document.querySelector('.seriesItem');
+var opticalTitle = document.querySelector('.optical');
+var sunglassesTitle = document.querySelector('.sunglasses');
+var functionalTitle = document.querySelector('.functional');
+seriesItem.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (e.target.textContent === 'OPTICAL') {
+    opticalTitle.setAttribute('class', 'border-show');
+    sunglassesTitle.removeAttribute('class');
+    functionalTitle.removeAttribute('class');
+  } else if (e.target.textContent === 'SUNGLASSES') {
+    sunglassesTitle.setAttribute('class', 'border-show');
+    opticalTitle.removeAttribute('class');
+    functionalTitle.removeAttribute('class');
+  } else {
+    functionalTitle.setAttribute('class', 'border-show');
+    opticalTitle.removeAttribute('class');
+    sunglassesTitle.removeAttribute('class');
+  }
 });
 //# sourceMappingURL=all.js.map

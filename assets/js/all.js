3c3c7c0a29e0f797287c8138c8b433cb69c7d8a3
您的姓名 <98501671+Linlaose@users.noreply.github.com>
2022-07-22 02:51:09 +0800

    Update 2022-07-21T18:51:04.467Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -10,35 +10,52 @@ var functionalTitle = document.querySelector('.functional'); // 經過 querySele
 var opticalImage = document.querySelectorAll('.opticalImageHeader');
 var celluloid = document.querySelector('.titleCelluloid');
 var opticalList = document.querySelector('.opticalProductList');
-var sunglassesList = document.querySelector('.sunglassesList'); // 觸發產品頁籤時加上 border
+var sunglassesList = document.querySelector('.sunglassesList');
+var labelText = document.querySelector('.district > span');
+var ulList = document.querySelector('.storeList');
+var selectList = document.querySelector('#card'); // 觸發產品頁籤
 
-seriesItem.addEventListener('click', function (e) {
-  e.preventDefault();
+if (seriesItem) {
+  //先加上 if 判斷，是為了避免頁面切換之後找不到原有的元素而跳錯誤
+  seriesItem.addEventListener('click', function (e) {
+    e.preventDefault();
 
-  if (e.target.textContent === 'OPTICAL') {
-    opticalTitle.setAttribute('class', 'borderShow');
-    sunglassesTitle.setAttribute('class', 'borderTransparent');
-    functionalTitle.setAttribute('class', 'borderTransparent');
-    opticalImage.forEach(function (i) {
-      i.removeAttribute('class');
-    });
-    celluloid.textContent = 'Celluloid Combi';
-    opticalList.setAttribute('class', 'opticalProductList');
-    sunglassesList.setAttribute('class', 'listHide');
-  } else if (e.target.textContent === 'SUNGLASSES') {
-    sunglassesTitle.setAttribute('class', 'borderShow');
-    opticalTitle.setAttribute('class', 'borderTransparent');
-    functionalTitle.setAttribute('class', 'borderTransparent');
-    opticalImage.forEach(function (i) {
-      i.setAttribute('class', 'imgHide');
-    });
-    celluloid.textContent = '2020 NEW COLLECTION';
-    opticalList.setAttribute('class', 'listHide');
-    sunglassesList.setAttribute('class', 'sunglassesProductList');
-  } else {
-    functionalTitle.setAttribute('class', 'borderShow');
-    opticalTitle.setAttribute('class', 'borderTransparent');
-    sunglassesTitle.setAttribute('class', 'borderTransparent');
-  }
-});
+    if (e.target.textContent === 'OPTICAL') {
+      opticalTitle.setAttribute('class', 'borderShow');
+      sunglassesTitle.setAttribute('class', 'borderTransparent');
+      functionalTitle.setAttribute('class', 'borderTransparent');
+      opticalImage.forEach(function (i) {
+        i.removeAttribute('class');
+      });
+      celluloid.textContent = 'Celluloid Combi';
+      opticalList.setAttribute('class', 'opticalProductList');
+      sunglassesList.setAttribute('class', 'listHide');
+    } else if (e.target.textContent === 'SUNGLASSES') {
+      sunglassesTitle.setAttribute('class', 'borderShow');
+      opticalTitle.setAttribute('class', 'borderTransparent');
+      functionalTitle.setAttribute('class', 'borderTransparent');
+      opticalImage.forEach(function (i) {
+        i.setAttribute('class', 'imgHide');
+      });
+      celluloid.textContent = '2020 NEW COLLECTION';
+      opticalList.setAttribute('class', 'listHide');
+      sunglassesList.setAttribute('class', 'sunglassesProductList');
+    } else {
+      functionalTitle.setAttribute('class', 'borderShow');
+      opticalTitle.setAttribute('class', 'borderTransparent');
+      sunglassesTitle.setAttribute('class', 'borderTransparent');
+    }
+  });
+} // 觸發門市據點的詳細資訊
+
+
+if (ulList) {
+  ulList.addEventListener('click', function (e) {
+    if (e.target.className === 'storeDetails') {
+      var store = e.target.previousElementSibling.previousElementSibling.textContent;
+      labelText.textContent = '選擇分店';
+      selectList.innerHTML = "<option value=\"taipei\">".concat(store, "</option>");
+    }
+  });
+}
 //# sourceMappingURL=all.js.map
